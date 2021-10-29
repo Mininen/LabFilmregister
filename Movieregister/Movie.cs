5491843792c90376a9859a56d2110a0693088035
Miriam <88656262+Mininen@users.noreply.github.com>
2021-10-29 15:31:32 +0200

    Add files via upload

---
+++ Movieregister/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movieregister
+{
+   public class Movie
+    {
+        public string title { get; set;}
+        public int year { get; set; }
+        public string genre { get; set; }
+        public int length { get; set; }
+        public double score { get; set; }
+    }
+}
